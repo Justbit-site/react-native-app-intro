f8b77621f3a5808647671c5c7f4cb515ed35a7d2
Santiago Molina Orozco <santi020k@gmail.com>
2017-12-08 17:56:33 -0500

    Update AppIntro.js

--- AppIntro.js
+++ AppIntro.js
@@ -77,7 +77,7 @@ const defaulStyles = {
     right: 0,
     flexDirection: 'row',
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: 'transparent',
   },
